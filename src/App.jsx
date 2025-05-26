9a4b8d61840118e3d5ea15f028fabf3bc475c61b
jhona <jhoalmanza52@gmail.com>
2025-05-26 10:11:30 -0500

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      
+      <header className=''>
+        <h1>Welcome to Ecommerce</h1>
+      </header>
+
+    </div>
+  )
+}
+
+export default App
